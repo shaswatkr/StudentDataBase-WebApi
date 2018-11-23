c5c568c443207274ec9eb47c575bd397df7bae90
Shaswat Kumar <shaswatkr96@gmail.com>
2018-11-23 18:13:41 +0530

    Uploaded WebApiConsumer
    Only uploaded some necessary files like, Controller, Views, Models, App_Start, Global.asax, etc.

---
+++ WebApiConsumer/Models/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiConsumer.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string FirstNameStu { get; set; }
+        public string LastNameStu { get; set; }
+    }
+}
